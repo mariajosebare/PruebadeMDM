1f0e74fea07161792777cc600734a85a25e3d03f
María José Bareño <47755417+mariajosebare@users.noreply.github.com>
2020-03-17 19:51:18 -0300

    login

--- app/src/main/java/com/example/pruebademdm/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/pruebademdm/ui/login/LoginActivity.java
@@ -37,50 +37,50 @@ public class LoginActivity extends AppCompatActivity {
         loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
-        final EditText usernameEditText = findViewById(R.id.container);
-        final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
-        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        //final EditText usernameEditText = findViewById(R.id.container);
+        //final EditText passwordEditText = findViewById(R.id.password);
+        //final Button loginButton = findViewById(R.id.login);
+        //final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
+        //loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
+            //@Override
+            //public void onChanged(@Nullable LoginFormState loginFormState) {
+                //if (loginFormState == null) {
+                    //return;
+                //}
+                //loginButton.setEnabled(loginFormState.isDataValid());
+                //if (loginFormState.getUsernameError() != null) {
+                    //usernameEditText.setError(getString(loginFormState.getUsernameError()));
+                //}
+                //if (loginFormState.getPasswordError() != null) {
+                    //passwordEditText.setError(getString(loginFormState.getPasswordError()));
+                //}
             }
-        });
+        //});
 
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
+        //loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+            //@Override
+            //public void onChanged(@Nullable LoginResult loginResult) {
+                //if (loginResult == null) {
+                    //return;
+                //}
+                //loadingProgressBar.setVisibility(View.GONE);
+                //if (loginResult.getError() != null) {
+                    //showLoginFailed(loginResult.getError());
+                //}
+                //if (loginResult.getSuccess() != null) {
+                    //updateUiWithUser(loginResult.getSuccess());
+                //}
+                //setResult(Activity.RESULT_OK);
 
                 //Complete and destroy login activity once successful
-                finish();
-            }
-        });
+                //finish();
+            //}
+        //});
 
-        TextWatcher afterTextChangedListener = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        //TextWatcher afterTextChangedListener = new TextWatcher() {
+            //@Override
+            //public void beforeTextChanged(CharSequence s, int start, int count, int after) {
                 // ignore
             }
 
