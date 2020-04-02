73ca161454b646057c789d97e51830da4e707f54
María José Bareño <47755417+mariajosebare@users.noreply.github.com>
2020-04-02 17:16:15 -0300

    Union de pantallas habilidad mach y chat

--- app/src/main/java/com/example/pruebademdm/main_log.java
+++ app/src/main/java/com/example/pruebademdm/main_log.java
@@ -23,43 +23,19 @@ import androidx.appcompat.app.AppCompatActivity;
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main_log);
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             //COnexión con XML
-            necesidad = (EditText)findViewById(R.id.textInput_necesidad);
+            /*necesidad = (EditText)findViewById(R.id.textInput_necesidad);
             spinner1 = (Spinner)findViewById(R.id.categorias);
 
             //Asignar valores al spinner
             String [] Categorías = ("Arte","Mecánica","Fotografía","Electricidad","Cocina","Belleza")
 
             ArrayAdapter <String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_spinner_dropdown_item, Categorías);
-            spinner1.setAdapter(adapter);
-
-
-=======
-=======
->>>>>>> 8d5695102ee5aa7c840e50d067b56d73606e5f04
-=======
->>>>>>> 8d5695102ee5aa7c840e50d067b56d73606e5f04
-=======
->>>>>>> 8d5695102ee5aa7c840e50d067b56d73606e5f04
+            spinner1.setAdapter(adapter);*/
             //Comienza el codigo para colocar icono en el action bar
             getSupportActionBar().setDisplayShowHomeEnabled(true);
             getSupportActionBar().setIcon(R.mipmap.ic_launcher);
             // Finaliza codigo icono action bar
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 8d5695102ee5aa7c840e50d067b56d73606e5f04
-=======
->>>>>>> 8d5695102ee5aa7c840e50d067b56d73606e5f04
-=======
->>>>>>> 8d5695102ee5aa7c840e50d067b56d73606e5f04
-=======
->>>>>>> 8d5695102ee5aa7c840e50d067b56d73606e5f04
         }
 
         // Comienza metodo boton buscar necesidad
