18c12e5b427ad6605708872843b20e8a76ba5624
Mathter <mathter@gmail.com>
2020-04-15 22:42:19 -0300

    Arreglado el spinner

--- app/src/main/java/com/example/pruebademdm/main_log.java
+++ app/src/main/java/com/example/pruebademdm/main_log.java
@@ -4,13 +4,19 @@ package com.example.pruebademdm;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import c.e.p.util.HttpUtils;
 import cz.msebera.android.httpclient.Header;
@@ -29,11 +35,29 @@ import cz.msebera.android.httpclient.Header;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
+            final main_log pantalla = this;
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main_log);
             HttpUtils.get("/necesidades", null, new AsyncHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                     try {
-                        JSONObject response_habilidades=new JSONObject(new String(responseBody));
+                        JSONArray response_habilidades=new JSONArray(new String(responseBody));
+                        Spinner categorias = findViewById(R.id.categorias);
+                        List<String> habilidades = new ArrayList<String>();
+                        for (int i = 0; i < response_habilidades.length(); i++) {
+                            JSONObject habilidad = response_habilidades.getJSONObject(i);
+                            habilidades.add(habilidad.getString("nombre"));
+                        }
+                        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(pantalla,
+                                android.R.layout.simple_spinner_item, habilidades);
+                        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        categorias.setAdapter(dataAdapter);
+
+                        //Comienza el codigo para colocar icono en el action bar
+                        getSupportActionBar().setDisplayShowHomeEnabled(true);
+                        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+                        // Finaliza codigo icono action bar
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -45,22 +69,6 @@ import cz.msebera.android.httpclient.Header;
 
                 }
             });
-
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main_log);
-            //COnexión con XML
-            /*necesidad = (EditText)findViewById(R.id.textInput_necesidad);
-            spinner1 = (Spinner)findViewById(R.id.categorias);
-
-            //Asignar valores al spinner
-            String [] Categorías = ("Arte","Mecánica","Fotografía","Electricidad","Cocina","Belleza")
-
-            ArrayAdapter <String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_spinner_dropdown_item, Categorías);
-            spinner1.setAdapter(adapter);*/
-            //Comienza el codigo para colocar icono en el action bar
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-            getSupportActionBar().setIcon(R.mipmap.ic_launcher);
-            // Finaliza codigo icono action bar
         }
 
         // Comienza metodo boton buscar necesidad
