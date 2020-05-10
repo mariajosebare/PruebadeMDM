290de9005bfaacb9ceea93dc88afdcd65103a202
Cecilia-94 <cjvs-94@hotmail.com>
2020-05-09 22:10:01 -0300

    prueba de notificaciones

---
+++ app/src/main/java/com/example/pruebademdm/Notificaciones.java
@@ -0,0 +1,14 @@
+package com.example.pruebademdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Notificaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notificaciones);
+    }
+}
